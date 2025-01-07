fdd956ffea62a8fece49cd7d7ee8459cc9e719ed
Mohamed Ali <mohamed.ali.mahmoud.ali.com>
2025-01-07 18:14:32 +0200

    add another comment to _document.tsx file"

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
 // use this file if you need to add custom meta tags for seo
+// right click on the page and view source to see the meta tags
 export default class CustomeDocument extends Document {
   render() {
     return (
