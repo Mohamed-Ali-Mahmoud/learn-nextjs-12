68fe96cda57103a11d674b2b8ca599ee82eb9664
Mohamed Ali <mohamed.ali.mahmoud.ali.com>
2025-01-08 00:11:31 +0200

    add comment to aboutpage in index.tsx file

---
+++ pages/aboutpage/index.tsx
@@ -0,0 +1,6 @@
+import styles from "./index.module.scss";
+
+// this page accessible at http://localhost:3000/aboutpage
+export default function AboutPages() {
+  return <div className={styles.redHeading}>About Page</div>;
+}
