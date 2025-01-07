021ffb2f35d59ff65e557a167518ebd837544626
Mohamed Ali <mohamed.ali.mahmoud.ali.com>
2025-01-07 23:57:41 +0200

    modified

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,23 @@
-// import style just for this page
-import styles from "../styles/globals.module.css";
+// import sass file just for this page
+import styles from "../styles/index.module.scss";
 
-function Headings(props) {
-  return <h1 className={styles.orangeHeading}>{props.heading}</h1>;
+// props for add any data to component
+// children for add any component inside component
+function Headings(props, children) {
+  return (
+    <h1 className={styles.greenHeading}>
+      {props.heading} {props.children}
+    </h1>
+  );
 }
 
 // this page acccessable at http://localhost:3000/
 export default function Home() {
   return (
     <div>
-      <Headings heading="Hello from index.tsx File" />
+      <Headings heading="Heading 1">
+        <span>Sub Heading</span>
+      </Headings>
     </div>
   );
 }
