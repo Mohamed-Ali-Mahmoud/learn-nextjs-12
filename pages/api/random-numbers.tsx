d3e208ffeac3f4fa905dd06729b867c9a69991a8
Mohamed Ali <mohamed.ali.mahmoud.ali.com>
2025-01-08 01:18:58 +0200

    add res and req methods

--- pages/api/random-numbers.tsx
+++ pages/api/random-numbers.tsx
@@ -5,5 +5,32 @@ import { NextApiRequest, NextApiResponse } from "next";
 // this page accessible at http://localhost:3000/api/random-numbers
 // this page should not return any raect component, it should return json data
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ num: Math.floor(Math.random() * 10) });
+  // go to the console and run the following command
+  // and back to the terminal and you will see the name you entered in the console
+  // result: REQUEST BODY { name: 'John Doe' }
+
+  // fetch("http://localhost:3000/api/random-numbers", {
+  //   method: "POST",
+  //   headers: { "Content-Type": "application/json" },
+  //   body: JSON.stringify({ name: "John Doe" }),
+  // });
+
+  console.log("REQUEST BODY", req.body);
+
+  // got ot network tab in the browser and you will see the header "X-Custome-Header" with the value "we are open to higher pepole"
+  res.setHeader("X-Custome-Header", "we are open to higher pepole");
+
+  // got ot network tab in the browser and you will see the header "Set-Cookie" with the value "areyouprogrammer=true"
+  // and then go to console and type document.cookie and you will see "areyouprogrammer=true"
+  res.setHeader("Set-Cookie", "areyouprogrammer=true");
+
+  // go to network tab in the browser and you will see the status code 404
+  //res.statusCode = 404;
+
+  // got to localhost:3000/api/random-numbers and you will send "how are you ?" to the browser
+  // it similar to res.send("how are you ?") but it end the response
+  //res.end("how are you ?");
+
+  // got to localhost:3000/api/random-numbers and you will send "Hello World" to the browser
+  res.send("Hello World");
 }
