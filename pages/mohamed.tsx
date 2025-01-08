3b1b2274c37afe39587e9eb9b8ffc2f670797a94
Mohamed Ali <mohamed.ali.mahmoud.ali.com>
2025-01-08 03:25:14 +0200

    add comment

---
+++ pages/mohamed.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+// when craeting a new page, you need to export a function that returns a JSX element
+// this page will create a new route in the application
+// this element will be rendered as a page
+// should be  a react component
+// this page accessible at http://localhost:3000/mohamed
+export default function Mohamed() {
+  return <div>Hello from Mohamed Page</div>;
+}
